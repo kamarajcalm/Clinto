efc64382aafc6949bb5d1a09aad78a6b79eaf9b5
arun1927 Siamintern <arunsiamintern@gmail.com>
2021-08-06 11:50:19 +0530

    dw

--- AppSettings.js
+++ AppSettings.js
@@ -1,7 +1,7 @@
 const fontFamily = "openSans"
 const themeColor ="#3F4450"
-const url = 'http://192.168.29.98:8000'
-// const url = 'https://uportal.in'
+// const url = 'http://192.168.29.98:8000'
+const url = 'https://uportal.in'
 const wampServer = 'wss://ws.epsilonai.com/ws'
 const TextInput = "#E0E3E6"
 export default {
